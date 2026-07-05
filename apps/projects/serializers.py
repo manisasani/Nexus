27c53e809b25c6bd8dbe09ec54df6f01331a570c
mani <manisasani2005@gmail.com>
2026-07-05 15:13:28 +0330

    feat(projects): add proposal serializers with marketplace validation

--- apps/projects/serializers.py
+++ apps/projects/serializers.py
@@ -1,7 +1,8 @@
 from datetime import date
+from sqlite3 import IntegrityError
 
 from rest_framework import serializers
-from apps.projects.models import Project
+from apps.projects.models import Project, Proposal
 
 
 class ProjectReadSerializer(serializers.ModelSerializer):
@@ -57,3 +58,73 @@ class ProjectUpdateSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError('Budget must be greater than zero.')
 
     
+class ProposalReadSerializer(serializers.ModelSerializer):
+    freelancer_email = serializers.EmailField(
+        source="freelancer.email",
+        read_only=True
+    )
+    class Meta:
+        model = Proposal
+        fields = ('id', 'project', 'freelancer', 'bid_amount', 'cover_letter', 'status', 'created_at', 'updated_at')
+        read_only_fields = ('freelancer','freelancer_email','status')
+
+
+class ProposalCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Proposal
+        fields = ('bid_amount', 'cover_letter')
+    
+    def validate_bid_amount(self, bid_amount):
+        if bid_amount <= 0:
+            raise serializers.ValidationError('Bid amount must be greater than zero.')
+        return bid_amount
+    
+    def validate(self, data):
+        request = self.context['request']
+        project = self.context['project']
+        user = request.user
+
+        if user.role != 'FREELANCER':
+            raise serializers.ValidationError('Only freelancer can submit proposals.')
+        
+        if project.status != Project.Status.OPEN:
+            raise serializers.ValidationError('You can only apply to OPEN projects.')
+        
+        if project.owner_id == user.id:
+            raise serializers.ValidationError('You cannot submit a proposal to you own project.')
+        
+        return data
+
+    
+    def create(self, validated_data):
+        request = self.context['request']
+        project = self.context['project']
+
+        try:
+            return Proposal.objects.create(
+                project=project,
+                freelancer=request.user,
+                **validated_data
+            )
+        except IntegrityError:
+            raise serializers.ValidationError(
+                'You have already a proposal for ths project.'
+            )
+
+class ProposalUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Proposal
+        fields = ('bid_amount', 'cover_letter')
+    
+    def validate(self, data):
+        instance = self.instance
+
+        if instance.status != Proposal.Status.PENDING:
+            raise serializers.ValidationError('You can only edit proposal that are PENDING.')
+        
+        return data
+    
+    def validate_bid_amount(self, bid_amount):
+        if bid_amount <= 0:
+            raise serializers.ValidationError('Bid amount must be greater than zero.')
+        return bid_amount
