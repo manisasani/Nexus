ded16e97968fa278ef87a0bc65c2cbc2331ed580
mani <manisasani2005@gmail.com>
2026-07-11 17:58:27 +0330

    feat(docker): add docker-compose.yml with web and db services

--- config/settings/local.py
+++ config/settings/local.py
@@ -15,5 +15,12 @@ ALLOWED_HOSTS = env.list(
 )
 
 DATABASES = {
-    "default": env.db("DATABASE_URL")
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": env("DB_NAME"),
+        "USER": env("DB_USER"),
+        "PASSWORD": env("DB_PASSWORD"),
+        "HOST": env("DB_HOST", default="127.0.0.1"),
+        "PORT": env("DB_PORT", default="5432"),
+    }
 }
